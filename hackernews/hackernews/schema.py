a7d7945f8a553599b5a50b32b638c18bde8cd71a
Saakshaat <saakshaat2001@gmail.com>
2020-10-10 00:22:24 -0700

    Create mutation class from Mutation class from links.schema

--- hackernews/hackernews/schema.py
+++ hackernews/hackernews/schema.py
@@ -7,4 +7,8 @@ class Query(link_schema.Query, graphene.ObjectType):
     pass
 
 
-schema = graphene.Schema(query=Query)
+class Mutation(link_schema.Mutation, graphene.ObjectType):
+    pass
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
